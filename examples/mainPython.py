547cc228fbdf56a9be548672fe409090bbe412ff
Marcin Balicki <simpletoforget@gmail.com>
2014-08-04 09:00:32 -0400

    sawNDITracker - mainPython.py:  add required includes for cisst data types. Remove the linux platform fix that caused seg faults when cisstCommonPython was imported. Add comments. Add functional example to initialize the tracker and get tracker body positions.

--- examples/mainPython.py
+++ examples/mainPython.py
@@ -3,37 +3,70 @@ import platform
 import sys
 import time
 
-if platform.system() == 'Linux':
-    sys.setdlopenflags(sys.getdlopenflags() | 8)  # RTLD_GLOBAL = 0x08
-import cisstMultiTaskPython as mts
+from cisstCommonPython import *
+from cisstVectorPython import *
+from cisstOSAbstractionPython import *
+from cisstMultiTaskPython import *
+from cisstParameterTypesPython import *
+from cisstStereoVisionPython import *
 
 name = 'My Tracker'
 period = 0.01
+#load the NDI config file located in the same directory as this python script.
 configuration = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configNDITracker.xml')
 
-manager = mts.mtsManagerLocal.GetInstance()
+manager = mtsManagerLocal.GetInstance()
 manager.CreateAllAndWait(5.0)
 manager.StartAllAndWait(5.0)
 
-proxy = mts.mtsComponentWithManagement('{}Proxy'.format(name))
+proxy = mtsComponentWithManagement('{}Proxy'.format(name))
 manager.AddComponent(proxy)
 proxy.CreateAndWait(5.0)
 time.sleep(0.5)
 
 services = proxy.GetManagerComponentServices()
+
+#create an instance of the tracker
 result = services.Load('sawNDITracker')
 assert result, 'Failed to load {} using component services'.format('sawNDITracker')
 
-args = mts.mtsTaskPeriodicConstructorArg(name, period)
+args = mtsTaskPeriodicConstructorArg(name, period)
 result = services.ComponentCreate('mtsNDISerial', args)
 assert result, 'Failed to create {} of type {}'.format(name, 'mtsNDISerial')
 
 component = manager.GetComponent(name)
-# component.Configure(configuration)
+component.Configure(configuration)
 
+#create the main interface to the tracker
 controller = proxy.AddInterfaceRequiredAndConnect((name, 'Controller'))
 
 component.CreateAndWait(5.0)
 component.StartAndWait(5.0)
 
-print controller.IsTracking()
+#see if the device is actually connected
+controller.Beep(1)
+time.sleep(1.0)
+controller.Beep(2)
+
+#initialize controller
+controller.PortHandlesInitialize()
+controller.PortHandlesQuery()
+controller.PortHandlesEnable()
+time.sleep(0.5)
+
+#create an interface for the tracked pointer body
+trackedBodyName = 'Pointer'
+trackedBody = proxy.AddInterfaceRequiredAndConnect((name, trackedBodyName))
+
+#enable tracking
+controller.ToggleTracking(True)
+controller.Beep(2)
+
+print 'Controller tracking status: ' + str(controller.IsTracking())
+
+while True:
+    pose = trackedBody.GetPositionCartesian()
+    if pose.GetValid():  # if visible
+        print pose.Position().Translation()
+    else: 
+        print trackedBodyName + 'is not visible'
