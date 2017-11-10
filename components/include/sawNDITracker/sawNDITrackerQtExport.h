b87367424a6904bc3e2763961baa0712fe8b4496
Anton Deguet <anton.deguet@jhu.edu>
2017-11-10 15:59:51 -0500

    Renamed export header file for Qt library

--- components/include/sawNDITracker/sawNDITrackerQtExport.h
+++ components/include/sawNDITracker/sawNDITrackerQtExport.h
@@ -2,11 +2,10 @@
 /* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
 
 /*
-
   Author(s):  Anton Deguet, Ali Uneri
   Created on: 2009-10-13
 
-  (C) Copyright 2009-2012 Johns Hopkins University (JHU), All Rights Reserved.
+  (C) Copyright 2009-2017 Johns Hopkins University (JHU), All Rights Reserved.
 
 --- begin cisst license - do not edit ---
 
