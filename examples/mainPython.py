9c776d1be3c5b6bf5e358f1a29851b82d74369ef
Anton Deguet <anton.deguet@jhu.edu>
2017-11-28 18:19:51 -0500

    python example: work in progress, kind of works but need to use arguments to set serial port and config file'

--- examples/mainPython.py
+++ examples/mainPython.py
@@ -9,8 +9,8 @@ from cisstParameterTypesPython import *
 
 name = 'My Tracker'
 period = 0.01
-#load the NDI config file located in the same directory as this python script.
-configuration = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configNDITracker.xml')
+# load the NDI config file located in the same directory as this python script.
+configuration = '/home/adeguet1/catkin_ws/src/cisst-saw/sawNDITracker/share/ndi-active-tools.json'
 
 manager = mtsManagerLocal.GetInstance()
 manager.CreateAllAndWait(5.0)
@@ -23,35 +23,33 @@ time.sleep(0.5)
 
 services = proxy.GetManagerComponentServices()
 
-#create an instance of the tracker
+# create an instance of the tracker
 result = services.Load('sawNDITracker')
 assert result, 'Failed to load {} using component services'.format('sawNDITracker')
 
-args = mtsTaskPeriodicConstructorArg(name, period)
+args = mtsTaskPeriodicConstructorArg(name, period, False, 256)
 result = services.ComponentCreate('mtsNDISerial', args)
 assert result, 'Failed to create {} of type {}'.format(name, 'mtsNDISerial')
 
 component = manager.GetComponent(name)
 component.Configure(configuration)
 
-#create the main interface to the tracker
+# create the main interface to the tracker
 controller = proxy.AddInterfaceRequiredAndConnect((name, 'Controller'))
 
 component.CreateAndWait(5.0)
 component.StartAndWait(5.0)
 
-#see if the device is actually connected
+# initialize controller
+controller.Connect("/dev/ttyS0")
+time.sleep(2.0)
+
+# see if the device is actually connected
 controller.Beep(1)
 time.sleep(1.0)
 controller.Beep(2)
 
-#initialize controller
-controller.PortHandlesInitialize()
-controller.PortHandlesQuery()
-controller.PortHandlesEnable()
-time.sleep(0.5)
-
-#create an interface for the tracked pointer body
+# create an interface for the tracked pointer body
 trackedBodyName = 'Pointer'
 trackedBody = proxy.AddInterfaceRequiredAndConnect((name, trackedBodyName))
 
@@ -65,5 +63,5 @@ while True:
     pose = trackedBody.GetPositionCartesian()
     if pose.GetValid():  # if visible
         print pose.Position().Translation()
-    else: 
+    else:
         print trackedBodyName + 'is not visible'
